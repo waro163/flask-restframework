2c62ac7403acfc6dcce3f18f485817a226ea5107
waro163 <waro163@163.com>
2021-08-26 22:02:07 +0800

    fix test_init

--- test_init.py
+++ test_init.py
@@ -1,22 +1,7 @@
-from flask import Flask
-import unittest
+from .test import BaseTest
 from . import RestFrame
 
-class TestInitApp(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.app = Flask(__name__)
-        return super().setUpClass()
-
-    def setUp(self) -> None:
-        self.context = self.app.test_request_context()
-        self.context.push()
-        return super().setUp()
-    
-    def tearDown(self) -> None:
-        self.context.pop()
-        return super().tearDown()
+class TestInitApp(BaseTest):
     
     def test_init_app(self):
         # self.app.config.from_pyfile()
