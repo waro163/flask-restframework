60db52a372c47a41a271e3ff30ae96f38c62b293
waro163 <waro163@163.com>
2021-08-26 22:01:41 +0800

    add BaseTest class in test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import unittest
+
+class BaseTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.app = Flask(__name__)
+        return super().setUpClass()
+
+    def setUp(self) -> None:
+        self.context = self.app.test_request_context()
+        self.context.push()
+        return super().setUp()
+    
+    def tearDown(self) -> None:
+        self.context.pop()
+        return super().tearDown()
