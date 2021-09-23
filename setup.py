d61851cd0e7952220ff2affd2d2f05777cbb53a6
waro163 <waro163@163.com>
2021-09-23 15:12:36 +0800

    modify the project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='flask-restframework',
+    name='flask-rest-framework',
     version='0.0.1',
     license='MIT',
     description='',
