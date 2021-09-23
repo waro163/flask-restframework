9c6bb222abafe24d82c985c6eec882442216871a
waro163 <waro163@163.com>
2021-09-23 12:44:03 +0800

    fix the classifiers in setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     ],
     classifiers=[
         'Environment :: Web Environment',
-        'Framework :: flask :: 2.0',
+        'Framework :: Flask'
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'License :: OSI Approved :: MIT License',
