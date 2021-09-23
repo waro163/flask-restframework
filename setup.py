13b8e4b0c71d7399275dec31a2334e5aa4b865ae
waro163 <waro163@163.com>
2021-09-23 12:39:37 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='flask-restframework',
+    version='0.0.1',
+    license='MIT',
+    description='',
+    author='waro163',
+    author_email='waro163@163.com',
+    url='https://github.com/waro163/flask-restframework',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type = 'text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    python_requires = '>=3.5',
+    install_requires=[
+        'flask>=2.0.0',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: flask :: 2.0',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
