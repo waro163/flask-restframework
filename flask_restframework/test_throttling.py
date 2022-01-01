93cff7d1ee0e609213a594bd4abf63a87a38966b
waro163 <waro163@163.com>
2022-01-01 16:11:03 +0800

    add testcase for UserRateThrottle

--- flask_restframework/test_throttling.py
+++ flask_restframework/test_throttling.py
@@ -2,7 +2,7 @@ from flask import g
 from unittest import mock
 from . import RestFramework
 from .base_test import BaseTest
-from .throttling import AnonRateThrottle
+from .throttling import AnonRateThrottle, UserRateThrottle
 
 class MockCache:
     def __init__(self) -> None:
@@ -47,3 +47,31 @@ class TestAnonRateThrottle(BaseTest):
         # import pdb; pdb.set_trace()
         throttle_result = anon_throttle.allow_request()
         self.assertTrue(throttle_result)
+
+class TestUserRateThrottle(BaseTest):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cache = MockCache()
+        rf = RestFramework()
+        rf.init_app(cls.app, cache)
+
+    @mock.patch('flask_restframework.throttling.g',current_user=None)
+    def test_forbiden_for_anon_user(self, *args):
+        anon_throttle = UserRateThrottle("0/s")
+        throttle_result = anon_throttle.allow_request()
+        self.assertFalse(throttle_result)
+
+    @mock.patch('flask_restframework.throttling.g',current_user=MockUser("1234abcd"))
+    def test_forbiden_for_user(self, *args):
+        anon_throttle = UserRateThrottle("0/s")
+        throttle_result = anon_throttle.allow_request()
+        self.assertFalse(throttle_result)
+
+    @mock.patch('flask_restframework.throttling.g',current_user=MockUser("1234abcd"))
+    def test_allow_then_forbiden(self, *args):
+        anon_throttle = UserRateThrottle("1/m")
+        throttle_result = anon_throttle.allow_request()
+        self.assertTrue(throttle_result)
+        throttle_result = anon_throttle.allow_request()
+        self.assertFalse(throttle_result)
